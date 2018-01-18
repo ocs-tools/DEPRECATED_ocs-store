7e7268967acc6af21bab6bcb0e8000604ab44f9f
Akira Ohgaki <akiraohgaki@gmail.com>
2018-01-18 15:32:27 +0900

    Update build-docker.sh

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -18,7 +18,6 @@ TRANSFERLOG="${PROJDIR}/transfer.log"
 install_nodejs() {
     npm cache clean
     npm install n -g
-    #n stable
     n lts
     ln -sf /usr/local/bin/node /usr/bin/node
     ln -sf /usr/local/bin/node /usr/bin/nodejs
@@ -44,17 +43,25 @@ build_flatpak() {
 }
 
 build_appimage() {
-    # docker-image: ubuntu:16.04
+    # docker-image: ubuntu:14.04
+
     apt update -qq
+    apt -y install software-properties-common
     apt -y install curl git nodejs npm
-
-    apt -y install build-essential qt5-default libqt5websockets5-dev
-    apt -y install cmake libssl-dev libcurl4-gnutls-dev libxpm-dev
-
-    apt -y install file
-
-    apt -y install libgconf-2-4 libxss1
+    #apt -y install build-essential qt5-default libqt5websockets5-dev
+    #apt -y install cmake libssl-dev libcurl4-gnutls-dev libxpm-dev
     apt -y install libssl1.0.0 zlib1g
+    apt -y install libgconf-2-4 libxss1
+
+    add-apt-repository -y ppa:beineri/opt-qt593-trusty
+    echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_14.04/ /' > /etc/apt/sources.list.d/curl-httponly.list
+    curl -fsSL https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_14.04/Release.key | apt-key add -
+    apt update -qq
+
+    apt -y install build-essential libgl1-mesa-dev qt59base qt59websockets
+
+    curl -fsSL https://cmake.org/files/v3.10/cmake-3.10.0-rc5-Linux-x86_64.tar.gz | tar -xz --strip-components=1 -C /
+    apt -y install libssl-dev libcurl3 libcurl3-gnutls libcurl4-gnutls-dev libxpm-dev
 
     install_nodejs
 
@@ -62,7 +69,7 @@ build_appimage() {
     export HOME="/home/${PKGUSER}"
     chown -R ${PKGUSER}:${PKGUSER} "${PROJDIR}"
 
-    su -c "sh "${BUILDSCRIPT}" ${BUILDTYPE}" ${PKGUSER}
+    su -c "source /opt/qt59/bin/qt59-env.sh && sh "${BUILDSCRIPT}" ${BUILDTYPE}" ${PKGUSER}
 
     transfer_file "$(find "${PROJDIR}/build_"*${BUILDTYPE} -type f -name "${PKGNAME}*.AppImage")"
 }
